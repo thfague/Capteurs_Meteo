600737a0473d608ea4fb90f6811201766471f78e
jeanglade <jeanglade@IUTCLINFA1815>
2018-11-23 17:31:32 +0100

    Avancement page accueil

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,33 +1,80 @@
 package sample;
+
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
-import javafx.geometry.Pos;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.layout.GridPane;
 import javafx.stage.Stage;
 import java.io.IOException;
+import javafx.scene.text.Text;
+import javafx.event.EventHandler;
 
 public class Controller {
     @FXML
-    public void ClickCapteur(ActionEvent actionEvent) {
-        Button button1 = new Button("Digital");
-        button1.setAlignment(Pos.TOP_RIGHT);
-        
-    }
+    GridPane gridAcceuil;
 
+        //Liste des capteurs
+        /*List<Capteur> listCapteur = new ArrayList<>();
+        listCapteur.add(new Digital(20));
+        listCapteur.add(new Digital(6));
+        listCapteur.add(new Digital(25));*/
+        //Création et ajout d'un bouton pour chaque capteur
+        /*for (int i = 0; i<listCapteur.size(); i++) {
+            Button buttonCapteur = new Button("Capteur " + (i + 1));
+            buttonCapteur.onActionProperty();
+            grid.add(buttonCapteur,0,i);
+        }*/
 
     @FXML
-    protected void ClickOpen(ActionEvent actionEvent) {
+    public void ClickCapteur1(ActionEvent actionEvent) {
+        Text text = new Text("Capteur 1");
+        gridAcceuil.add(text, 1, 0 );
+        AffichageBouton();
+    }
 
-        /*NewWindow("digital.fxml", "Affichage format Digital");
-        NewWindow("thermo.fxml", "Affichage format Thermomètre");
-        NewWindow("imgMeteo.fxml", "Affichage format Image");*/
+    @FXML
+    public void ClickCapteur2(ActionEvent actionEvent) {
+        Text text = new Text("Capteur 2");
+        gridAcceuil.add(text, 1, 0 );
+        AffichageBouton();
+    }
+
+    @FXML
+    public void ClickCapteur3(ActionEvent actionEvent) {
+        Text text = new Text("Capteur 3");
+        gridAcceuil.add(text, 1, 0 );
+        AffichageBouton();
+    }
+
+    //Création et ajout d'un bouton pour chaque type d'affichage
+    public void AffichageBouton() {
+        Button buttonDigit = new Button("Affichage digital");
+        gridAcceuil.add(buttonDigit, 1, 1 );
+        /*buttonDigit.setOnAction(new EventHandler<ActionEvent>() {
+                                    @Override public void handle(ActionEvent e) {
+                                        NewWindow("digital.fxml", "Affichage format Digital");
+                                    }
+                                });*/
+        Button buttonThermo = new Button("Affichage par thermomètre");
+        gridAcceuil.add(buttonThermo, 1, 2 );
+        Button buttonImg = new Button("Affichage imagé");
+        gridAcceuil.add(buttonImg, 1, 3 );
+    }
+
+    public void OpenDigit() {
 
     }
 
+    public void OpenThermo() {
+        NewWindow("thermo.fxml", "Affichage format Thermomètre");
+    }
+
+    public void OpenImg() {
+        NewWindow("imgMeteo.fxml", "Affichage format Image");
+    }
 
     public void NewWindow(String nameFile, String title)
     {
@@ -35,7 +82,7 @@ public class Controller {
             Parent root = FXMLLoader.load(getClass().getResource(nameFile));
             Stage stage = new Stage();
             stage.setTitle(title);
-            stage.setScene(new Scene(root, 300, 275));
+            stage.setScene(new Scene(root, 400, 275));
             stage.show();
         }
         catch (IOException e) {
--- src/sample/Main.java
+++ src/sample/Main.java
@@ -1,41 +1,21 @@
 package sample;
 
-import Class_Metier.*;
 import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
-import javafx.geometry.Pos;
-import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.layout.GridPane;
 import javafx.stage.Stage;
-
-import java.util.ArrayList;
-import java.util.List;
-
+import javafx.scene.Parent;
+import javafx.fxml.FXMLLoader;
 
 public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        GridPane grid = new GridPane();
-        grid.setHgap(20);
-        grid.setVgap(20);
-        List<Capteur> listCapteur = new ArrayList<>();
-        listCapteur.add(new Digital(20));
-        listCapteur.add(new Digital(6));
-        listCapteur.add(new Digital(18));
-        for (int i = 0; i<?????(listCapteur); i++) { //Créer et afficher un bouton pour chaque capteur pour ensuite cliquer dessus et choisir le type d'affichage, il faut avoir la longueur de la liste
-            Button button = new Button("Test");
-            grid.add(button,0,i);
-        }
-
-        //Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+        //Fenêtre d'accueil
+        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
         primaryStage.setTitle("Meteo");
-        primaryStage.setScene(new Scene(grid, 800, 650)); //Si tu veux réutiliser le root pour utiliser le fxml, enlève le commentaire et change "grid" en "root"
+        primaryStage.setScene(new Scene(root, 400, 325));
         primaryStage.show();
     }
-
 
     public static void main(String[] args) {
         launch(args);
