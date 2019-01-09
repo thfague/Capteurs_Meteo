fe4f9e85ba4e295b6240f6bf4aab94dd4f828256
Thibault FAGUE <Thibault.FAGUE@etu.uca.fr>
2019-01-09 14:32:24 +0100

    Ajout binding val capteur avec affichage thermo

--- src/Vue/AffichageCapteur/AffichageThermo.java
+++ src/Vue/AffichageCapteur/AffichageThermo.java
@@ -1,4 +1,19 @@
 package Vue.AffichageCapteur;
 
+import Class_Metier.Capteur.CapteurAbstrait;
+import javafx.fxml.FXML;
+import javafx.scene.control.Slider;
+
 public class AffichageThermo {
+    private CapteurAbstrait capt;
+
+    @FXML
+    private Slider thermoSlider;
+
+    public AffichageThermo(CapteurAbstrait c) { capt=c; }
+
+    @FXML
+    private void initialize(){
+        thermoSlider.valueProperty().bind(capt.valeurProperty());
+    }
 }
